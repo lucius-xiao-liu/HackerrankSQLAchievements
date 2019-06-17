7e80b340eb3475debcebab076ff6f09154a2bec9
lucius-xiao-liu <51760338+lucius-xiao-liu@users.noreply.github.com>
2019-06-17 11:19:04 -0700

    Update Revising The Select Query I .sql

--- Revising The Select Query I .sql
+++ Revising The Select Query I .sql
@@ -1,5 +1,4 @@
-// https://www.hackerrank.com/challenges/revising-the-select-query/submissions/code/108235334
-
+// https://www.hackerrank.com/challenges/revising-the-select-query/
 select * 
 from CITY 
 where COUNTRYCODE = 'USA' AND POPULATION > 100000
