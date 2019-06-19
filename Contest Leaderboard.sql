9ada97eec901615e64361709eb0275e55563a1f7
lucius-xiao-liu <51760338+lucius-xiao-liu@users.noreply.github.com>
2019-06-19 10:17:03 -0700

    Create Contest Leaderboard.sql

---
+++ Contest Leaderboard.sql
@@ -0,0 +1,13 @@
+// https://www.hackerrank.com/challenges/contest-leaderboard
+
+WITH score(hacker_id, challenge_id, score) as (
+select hacker_id,challenge_id, max(submissions.score) 
+from submissions 
+group by hacker_id, challenge_id
+)
+select hackers.hacker_id,hackers.name,sum(score.score) as maxscore
+from hackers
+inner join score on score.hacker_id = hackers.hacker_id
+group by hackers.hacker_id, hackers.name
+having sum(score.score)>0
+order by maxscore desc, hackers.hacker_id asc
