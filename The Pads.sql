9bebc4dd7d5de41a9033d507c88256e3a53c7f98
lucius-xiao-liu <51760338+lucius-xiao-liu@users.noreply.github.com>
2019-09-25 11:18:22 -0700

    Create The Pads.sql

---
+++ The Pads.sql
@@ -0,0 +1,8 @@
+SELECT concat(NAME,concat('(',concat(substring(OCCUPATION,1,1),')'))) 
+                                     FROM OCCUPATIONS 
+                                     ORDER BY NAME ASC;
+
+SELECT 'There are a total of ', count(OCCUPATION), concat(lower(occupation),'s.') 
+                                     FROM OCCUPATIONS 
+                                     GROUP BY OCCUPATION 
+                                     ORDER BY count(OCCUPATION), OCCUPATION ASC;
